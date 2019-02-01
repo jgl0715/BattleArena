c867632c34eb62ad9f5cdc231501bfcf6395b5eb
unknown <foreseenparadox@gmail.com>
2019-02-01 13:36:42 -0600

    Initial commit.

---
+++ desktop/src/battlearena/main/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package battlearena.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import battlearena.main.BattleArena;
+
+public class DesktopLauncher
+{
+	public static void main (String[] arg)
+	{
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BattleArena(), config);
+	}
+}
