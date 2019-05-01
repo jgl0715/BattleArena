71291b07559f5c7804dd44c357aed98926d22cd8
Kunal Samant <kunalnitin.samant@mavs.uta.edu>
2019-05-01 02:05:15 -0500

    Added silent sign-in

--- android/src/battlearena/main/AndroidLauncher.java
+++ android/src/battlearena/main/AndroidLauncher.java
@@ -1,6 +1,9 @@
 package battlearena.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+
 import battlearena.game.BattleArena;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -13,6 +16,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions.Builder;
 import com.google.android.gms.common.SignInButton;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class AndroidLauncher extends AndroidApplication
@@ -26,7 +32,9 @@ public class AndroidLauncher extends AndroidApplication
 		initialize(new BattleArena(), config);
 		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         GoogleSignInClient mGoogleSigninClient = GoogleSignIn.getClient(this, gso);
-        //SignInButton signInButton = findViewById(R.id.sign_int_button);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+                .requestScopes(Drive.SCOPE_APPFOLDER)
+                .build();
 	}
 
     @Override
@@ -38,5 +46,47 @@ public class AndroidLauncher extends AndroidApplication
 
     private void updateUI(GoogleSignInAccount account) {
 
+    }
+
+    private void signInSilently() {
+        GoogleSignInOptions signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN;
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            GoogleSignInAccount signedInAccount = account;
+        }
+        else{
+            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
+            signInClient.silentSignIn().addOnCompleteListener(this, new OnCompleteListener<GoogleSignInAccount>() {
+                @Override
+                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
+                    if (task.isSuccessful()) {
+                        // The signed in account is stored in the task's result.
+                        GoogleSignInAccount signedInAccount = task.getResult();
+                    } else {
+                        // Player will need to sign-in explicitly using via UI.
+                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
+                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
+                        // Interactive Sign-in.
+                    }
+                }
+            });
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        signInSilently();
+    }
+
+    private void startSignInIntent() {
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(this,
+                GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN);
+        Intent intent = signInClient.getSignInIntent();
+        //startActivityForResult(intent, RC_SIGN_IN);
     }
 }
